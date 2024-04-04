9c1be08940d1bb66a7fb91350eba71aeeeefbfde
Dambar Sing <programmer.gshi18@gmail.com>
2024-04-04 18:54:42 +0545

    home page responsive fixing

--- VRMS_Frontend/src/components/Footer/Footer.jsx
+++ VRMS_Frontend/src/components/Footer/Footer.jsx
@@ -21,29 +21,29 @@ const Footer = () => {
             <h5>Phone: <span> 082403067</span></h5>
             <h5 style={{ marginTop:"3px" }}>Email: <span>ito.babaimun@gmail.com</span></h5>
           </div>
-          <div className="foot-egovServices" style={{ marginLeft: "5vw" }}>
-            <h3 style={{ fontSize: "20px" }}>E-Gov Services</h3>
-            <li onClick={()=>Navigate('/birthRegistration')} style={{ fontSize: "14px", fontWeight: "normal" }}>
+          <div className="foot-egovServices">
+            <h3>E-Gov Services</h3>
+            <li onClick={()=>Navigate('/birthRegistration')}>
               Birth Registration
             </li>
-            <li onClick={()=>Navigate('/deathRegistration')} style={{ fontSize: "14px", fontWeight: "normal" }}>
+            <li onClick={()=>Navigate('/deathRegistration')}>
               Death Registration
             </li>
           </div>
-          <div className="important-links" style={{ marginLeft: "3vw" }}>
-            <h3 style={{ fontSize: "20px" }}>Important Links</h3>
-            <li style={{ fontSize: "14px", fontWeight: "normal" }} href="">
+          <div className="important-links">
+            <h3>Important Links</h3>
+            <li href="">
               Birth Certificate
             </li>
-            <li style={{ fontSize: "14px", fontWeight: "normal" }} href="">
+            <li href="">
               Death Certificate
             </li>
-            <li style={{ fontSize: "14px", fontWeight: "normal" }} href="">
+            <li href="">
               Have you lost Documents
             </li>
           </div>
-          <div className="location" style={{ marginLeft: "2.8vw" }}>
-            <h3 style={{ fontSize: "20px",marginBottom:"3px" }}>Office Location</h3>
+          <div className="location">
+            <h3 style={{marginBottom:"3px"}}>Office Location</h3>
             <iframe style={{ width: "25vw", height: "auto", style: "border:0" }}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3516.794754017949!2d82.12646007409299!3d28.183156804347846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39981d83cf10baa7%3A0x36e358ae202e448a!2sBabai%20Rural%20Municipality!5e0!3m2!1sen!2snp!4v1711190586246!5m2!1sen!2snp"
               allowfullscreen=""
@@ -53,7 +53,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-second flex">
-          <h5 style={{textAlign: "center",fontWeight: "lighter",fontSize: "12px",}}>Copyright &copy Babai Rural Municipality, Dang
+          <h5 style={{fontWeight:"lighter"}}>Copyright &copy Babai Rural Municipality, Dang
           </h5>
         </div>
       </div>
