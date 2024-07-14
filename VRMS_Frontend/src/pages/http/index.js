810cb86e8e883c948064ff49cf155117d9896587
Dambar Sing <programmer.gshi18@gmail.com>
2024-07-14 21:29:12 +0545

    baseURL for v2.vrms

--- VRMS_Frontend/src/pages/http/index.js
+++ VRMS_Frontend/src/pages/http/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:9000/api",
-    // baseURL:"https://v2-vrms.vercel.app/api",
+    // baseURL:"http://localhost:9000/api",
+    baseURL:"https://v2-vrms.vercel.app/api",
     headers:{
         "Content-Type":"application/json",
         Accept:"application/json"
